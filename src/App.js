00b3007dd3cc0c2f73804067a3002d64a5406cc7
ayushguptasah <a4ayush2gupta@gmail.com>
2022-04-18 17:22:45 +0530

    update

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import React, { Component } from "react";
 import Navbar from "./components/Navbar";
 import News from "./components/News";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 // import NewsItem from "./components/NewsItem";
 import LoadingBar from "react-top-loading-bar";
 
